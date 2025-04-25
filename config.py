dbae150562bb6b5fe239f113812ee52ffab72aeb
EksaranPhaoseryLaksmi <laksmi4554@gmail.com>
2025-04-26 01:08:21 +0700

    config

--- config.py
+++ config.py
@@ -1,6 +1,4 @@
 class Config:
     def __init__(self):
         self.DEBUG = True
-        self.SQLALCHEMY_DATABASE_URI='mysql+pymysql://root:bella14@localhost:3306/ap'
-        
-        
+        self.SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:IJRFHiyTaYXpvUIvqBuWzgFFifwoCtzm@crossover.proxy.rlwy.net:23617/railway'
